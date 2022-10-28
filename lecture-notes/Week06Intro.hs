d5451763f2b607edd68ca4247dbd49317b124803
Robert Atkey <robert.atkey@strath.ac.uk>
2022-10-28 13:11:10 +0100

    [ lecture-notes  ] Complete the Week 06 live notes

--- lecture-notes/Week06Intro.hs
+++ lecture-notes/Week06Intro.hs
@@ -159,6 +159,157 @@ numberList_v2 (x:xs) =
   numberList_v2 xs `andThen` (\ys ->
   returnSt ((x,i) : ys))))
 
+
+-- Simulating exceptions with 'Maybe' -- 'Nothing' means 'throw an exception'
+--                                       'Just a'  means 'return a'
+
+-- returnOk :: a -> Maybe a
+-- returnSt :: a -> State a
+
 -- ifOK    :: Maybe a -> (a -> Maybe b) -> Maybe b
 -- andThen :: State a -> (a -> State b) -> State b
---    --- VERY SIMILAR TYPES
+
+-- "do this and, with the result, do the next thing"
+
+
+
+{-   Part 6.4 : SIMULATING PRINTING (or OUTPUT) -}
+
+data Tree a
+  = Leaf
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+printAndSum :: Tree Int -> ([String], Int)
+printAndSum Leaf =
+  ([], 0)
+printAndSum (Node l x r) =
+  let (out1, lsum) = printAndSum l
+      out          = ["Doing " ++ show x]
+      (out2, rsum) = printAndSum r
+  in (out1 ++ out ++ out2, lsum + x + rsum)
+
+testTree :: Tree Int
+testTree = Node (Node Leaf 4 Leaf) 7 (Node Leaf 9 Leaf)
+
+data Printing a = MkPrinting [String] a
+  deriving Show
+
+returnPr :: a -> Printing a
+returnPr x = MkPrinting [] x
+
+andThenPrinting :: Printing a -> (a -> Printing b) -> Printing b
+--                 first job which may print 'output1' and return 'a'
+--                               second job requires an 'a', may print 'output2' and return 'b'
+--                                                    output1 ++ output2, return 'b'
+andThenPrinting (MkPrinting output1 a) k =
+  case k a of
+    MkPrinting output2 b ->
+      MkPrinting (output1 ++ output2) b
+
+
+printAndSum_v2 :: Tree Int -> Printing Int
+printAndSum_v2 Leaf =
+  returnPr 0
+--  ([], 0)
+printAndSum_v2 (Node l x r) =
+  printAndSum_v2 l              `andThenPrinting`
+    (\lsum ->
+       printStr ("Doing " ++ show x) `andThenPrinting`
+         (\() ->
+            printAndSum_v2 r              `andThenPrinting`
+              (\rsum ->
+                  returnPr (lsum + x + rsum))))
+
+printStr :: String -> Printing ()
+printStr s = MkPrinting [s] ()
+
+-- Another example of "do this, then do that"
+
+
+{-   Part 6.5 : PROCESSES -}
+
+data Process a
+  = End a
+  | Input (String -> Process a)
+  | Output String (Process a)
+
+example :: Process ()
+example =
+  Output "What is your name?"
+         (Input (\input ->
+             Output ("Hello " ++ input)
+                    (End ())))
+
+
+runProcess :: Process a -> IO a
+runProcess (End a)      = return a
+runProcess (Input k)    = do s <- getLine; runProcess (k s)
+runProcess (Output s p) = do putStrLn s; runProcess p
+
+returnProcess :: a -> Process a
+returnProcess x = End x
+
+{-
+                Input
+         /        |      \
+   Output "a"  End 5    Output "b"
+        |                |
+     End 1             End 4
+-}
+
+{-  \i -> Output (show i) (End ()) -}
+
+{-
+                Input
+         /        |            \
+   Output "a"  Output "5"    Output "b"
+        |         |             |
+   Output "1"   End ()       Output "4"
+        |                       |
+      End ()                  End ()
+
+-}
+
+
+process1 = Input (\input ->
+                    if input == "a" then Output "a" (End 1)
+                    else if input == "b" then End 5
+                    else Output "b" (End 4))
+
+process2 i = Output (show i) (End ())
+
+sequ :: Process a -> (a -> Process b) -> Process b
+sequ (End a)      k = k a
+sequ (Input p)    k = Input (\s -> sequ (p s) k)
+sequ (Output s p) k = Output s (sequ p k)
+
+output :: String -> Process ()
+output s = Output s (End ())
+
+input :: Process String
+input = Input (\s -> End s)
+
+example_v2 :: Process ()
+example_v2 =
+  output "What is your name?" `sequ` \() ->
+  input                       `sequ` \str ->
+  output ("Hello " ++ str)    `sequ` \() ->
+  returnProcess ()
+
+
+-- Four ways of "simulating side effects"
+--
+-- All have similar interfaces:
+--
+--   ifOK            :: Maybe a    -> (a -> Maybe b)    -> Maybe b
+--   andThen         :: State a    -> (a -> State b)    -> State b
+--   andThenPrinting :: Printing a -> (a -> Printing b) -> Printing b
+--   sequ            :: Process a  -> (a -> Process b)  -> Process b
+
+
+-- The common name is "Monad"
+
+-- class Monad m where
+--    return :: a -> m a
+--    (>>=)  :: m a -> (a -> m b) -> m b
